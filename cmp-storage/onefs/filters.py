186076d327efa32d83ca00e98b3826fbe7a9148b
thw <tanhaowen1996@gmail.com>
2022-11-18 18:34:11 +0800

    add region

--- cmp-storage/onefs/filters.py
+++ cmp-storage/onefs/filters.py
@@ -10,7 +10,8 @@ class NFSFilter(FilterSet):
     tenant_id = CharFilter(field_name='tenant_id', lookup_expr='icontains')
     tenant_name = CharFilter(field_name='tenant_name', lookup_expr='icontains')
     ip = CharFilter(field_name='ip', lookup_expr='icontains')
+    region = CharFilter(field_name='region', lookup_expr='icontains')
 
     class Meta:
         mode = NFS
-        filter = ('name', 'id', 'tenant_id', 'tenant_name', 'ip')
+        filter = ('name', 'id', 'tenant_id', 'tenant_name', 'region', 'ip')
