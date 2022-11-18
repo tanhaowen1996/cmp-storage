98daf7237d609f776f9156d6f4bfd90913f35187
thw <tanhaowen1996@gmail.com>
2022-11-18 16:30:14 +0800

    chage db

--- cmp-storage/onefs/admin.py
+++ cmp-storage/onefs/admin.py
@@ -5,5 +5,5 @@ from .models import NFS
 @admin.register(NFS)
 class NFSAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'subnet_id', 'tenant_id', 'cidr', 'vlan_id',
-                    'ip', 'network_id', 'status',
+                    'ip', 'network_id', 'status', 'region',
                     'nfs_id', 'quota_id')
