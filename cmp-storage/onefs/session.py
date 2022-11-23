7b52d54b2209dfa66f3f2155e1d004232c960aee
thw <tanhaowen1996@gmail.com>
2022-11-23 13:54:47 +0800

    delete isi_sdk_9_1_0

--- cmp-storage/onefs/session.py
+++ cmp-storage/onefs/session.py
@@ -1,5 +1,4 @@
 from requests.auth import HTTPBasicAuth
-import isi_sdk_9_1_0
 from cmp_storage.settings import ONEFS_USER, ONEFS_URL, ONEFS_PASSWORD
 
 
@@ -9,11 +8,3 @@ class OneFSMixin:
     def get_session():
         nfs_session = HTTPBasicAuth(ONEFS_USER, ONEFS_PASSWORD)
         return nfs_session
-        # configuration = isi_sdk_9_1_0.Configuration()
-        # configuration.username = ONEFS_USER
-        # configuration.password = ONEFS_PASSWORD
-        # configuration.verify_ssl = False
-        # # configure host
-        # configuration.host = ONEFS_URL
-        # api_client = isi_sdk_9_1_0.ApiClient(configuration)
-        # return api_client
