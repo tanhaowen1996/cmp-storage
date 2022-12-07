057e113eaa25241f2da94fb6cbf45ed6d02a4fb9
thw <tanhaowen1996@gmail.com>
2022-12-07 14:07:16 +0800

    fix bug

--- cmp-storage/onefs/serializers.py
+++ cmp-storage/onefs/serializers.py
@@ -15,7 +15,19 @@ class NFSSerializer(serializers.ModelSerializer):
     nfs_id = serializers.IntegerField(required=False)
     quota_id = serializers.CharField(required=False)
     region = serializers.CharField(required=False)
+    file_agreement = serializers.CharField(required=False)
+    file_size = serializers.CharField(required=False)
 
     class Meta:
         model = NFS
         fields = '__all__'
+
+
+class UpdateNFSSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(required=False)
+
+    class Meta:
+        model = NFS
+        fields = (
+            'name'
+        )
